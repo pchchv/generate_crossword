98b62cd923a8f697dd47a8778836aab1675fd7b0
pchchv <ipchchv@gmail.com>
2025-10-13 21:46:03 +0300

    feat: create CrosswordGeneratorApp widget

--- lib/widgets/crossword_generator_app.dart
+++ lib/widgets/crossword_generator_app.dart
@@ -3,6 +3,43 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers.dart';
 
+class CrosswordGeneratorApp extends StatelessWidget {
+  const CrosswordGeneratorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _EagerInitialization(
+      child: Scaffold(
+        appBar: AppBar(
+          titleTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          title: Text('Crossword Generator'),
+        ),
+        body: SafeArea(
+          child: Consumer(
+            builder: (context, ref, _) {
+              final wordListAsync = ref.watch(wordListProvider);
+              return wordListAsync.when(
+                data: (wordList) => ListView.builder(
+                  itemCount: wordList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text(wordList.elementAt(index)));
+                  },
+                ),
+                error: (error, stackTrace) => Center(child: Text('$error')),
+                loading: () => Center(child: CircularProgressIndicator()),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _EagerInitialization extends ConsumerWidget {
   const _EagerInitialization({required this.child});
   final Widget child;
