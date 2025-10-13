d0563b1324287854a469324cfb123775e694e782
pchchv <ipchchv@gmail.com>
2025-10-13 21:43:29 +0300

    feat: generate code for provider

---
+++ lib/providers.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+/// A provider for the wordlist to use when generating the crossword.
+
+@ProviderFor(wordList)
+const wordListProvider = WordListProvider._();
+
+/// A provider for the wordlist to use when generating the crossword.
+
+final class WordListProvider
+    extends
+        $FunctionalProvider<
+          AsyncValue<BuiltSet<String>>,
+          BuiltSet<String>,
+          FutureOr<BuiltSet<String>>
+        >
+    with $FutureModifier<BuiltSet<String>>, $FutureProvider<BuiltSet<String>> {
+  /// A provider for the wordlist to use when generating the crossword.
+  const WordListProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'wordListProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$wordListHash();
+
+  @$internal
+  @override
+  $FutureProviderElement<BuiltSet<String>> $createElement(
+    $ProviderPointer pointer,
+  ) => $FutureProviderElement(pointer);
+
+  @override
+  FutureOr<BuiltSet<String>> create(Ref ref) {
+    return wordList(ref);
+  }
+}
+
+String _$wordListHash() => r'07003444d7241448561b638500001e31610b4717';
